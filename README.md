b7bf64929308dca0544cab42532626b67dd51ebb
Damm Bro <144452060+YashDhirajOza@users.noreply.github.com>
2024-09-20 15:46:29 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# IntroGDG
+# Paralax-effect-with-gsap-scrolltrigger
+Paralax effect with gsap scrolltrigger
+
+DEMO - https://dmitrinaumov.github.io/Paralax-effect-with-gsap-scrolltrigger/ \
---
+++ js/js.js
@@ -0,0 +1,194 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+	'use strict';
+
+
+
+	Splitting();
+	luxy.init();
+	gsap.registerPlugin(ScrollTrigger);
+
+	const gTl = gsap.timeline();
+	gTl.from(".title .char", 1, { opacity: 0, yPercent: 130, stagger: 0.06, ease: "back.out" });
+	gTl.to(".header__img", 2, { clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)", scale: 1, ease: "expo.out" }, "-=1");
+	gTl.from(".header__marq", 2, { opacity: 0, yPercent: 100, ease: "expo.out" }, "-=1.5");
+
+	const gsapSq = gsap.utils.toArray('.section-title__square');
+	gsapSq.forEach((gSq, i) => {
+		const rotat = gsap.from(gSq, 3, { rotation: 720 });
+		ScrollTrigger.create({
+			trigger: gSq,
+			animation: rotat,
+			start: 'top bottom',
+			scrub: 1.9
+		});
+	});
+
+
+	//header
+	function header() {
+		gsap.to('.title_paralax', {
+			scrollTrigger: {
+				trigger: '.header',
+				start: 'top top',
+				scrub: 1.9
+			},
+			yPercent: -150
+		})
+		gsap.to('.header .stroke', {
+			scrollTrigger: {
+				trigger: '.header',
+				start: 'top top',
+				scrub: 1.9
+			},
+			xPercent: 50
+		})
+		gsap.to('.header__img', {
+			scrollTrigger: {
+				trigger: '.header',
+				start: 'top top',
+				scrub: 1.9
+			},
+			xPercent: -70
+		})
+		gsap.to('.header__img img', {
+			scrollTrigger: {
+				trigger: '.header',
+				start: 'top top',
+				scrub: 1.9
+			},
+			scale: 1.3
+		})
+		gsap.to('.header__marq-wrapp', {
+			scrollTrigger: {
+				trigger: '.header',
+				start: 'top top',
+				scrub: 1.9
+			},
+			xPercent: -50
+		})
+		gsap.to('.header__marq-star img', {
+			scrollTrigger: {
+				trigger: '.header',
+				start: 'top top',
+				scrub: 1.9
+			},
+			rotate: -720
+		})
+	}
+	header();
+
+
+	//about
+	function about() {
+		gsap.from('.about__img', {
+			scrollTrigger: {
+				trigger: '.about',
+				start: 'top bottom',
+				scrub: 1.9
+			},
+			yPercent: 80
+		})
+		gsap.from('.about__img img', {
+			scrollTrigger: {
+				trigger: '.about',
+				start: 'top bottom',
+				scrub: 1.9
+			},
+			scale: 1.6
+		})
+		gsap.to('.about__txt', {
+			scrollTrigger: {
+				trigger: '.about__wrapp',
+				start: 'top bottom',
+				scrub: 1.9
+			},
+			yPercent: 50
+		})
+	}
+	about();
+
+
+	//benefits
+	function benefits() {
+		gsap.from('.benefits__num', {
+			x: (i, el) => (1 - parseFloat(el.getAttribute('data-speed'))),
+			scrollTrigger: {
+				trigger: '.benefits__list',
+				start: 'top bottom',
+				scrub: 1.9
+			}
+		})
+	}
+	benefits();
+
+
+	//portfolio
+	function portfolio() {
+		gsap.from('.work__item, .work__item-num', {
+			y: (i, el) => (1 - parseFloat(el.getAttribute('data-speed'))),
+			scrollTrigger: {
+				trigger: '.work',
+				start: 'top bottom',
+				scrub: 1.9
+			}
+		})
+		gsap.from('.work__item-img img', {
+			scale: 1.6,
+			scrollTrigger: {
+				trigger: '.work__wrapp',
+				start: 'top bottom',
+				scrub: 1.9
+			}
+		})
+	}
+	portfolio();
+
+
+	//serv
+	function serv() {
+		gsap.from('.serv__item-arrow', {
+			x: (i, el) => (1 - parseFloat(el.getAttribute('data-speed'))),
+			scrollTrigger: {
+				trigger: '.serv__list',
+				start: 'top bottom',
+				scrub: 1.9
+			}
+		})
+	}
+	serv();
+
+
+	//footer
+	function footer() {
+		// Select all the span elements in the footer that spell "Yash"
+		const footerLetters = document.querySelectorAll('.footer__div span');
+		
+		// Animate each letter with GSAP
+		footerLetters.forEach((letter, index) => {
+		  gsap.fromTo(letter, 
+			{
+			  y: (index % 2 === 0 ? -50 : 50), // Alternate between moving up and down
+			  opacity: 0,
+			  rotation: 0
+			},
+			{
+			  y: 0,  // Bring back to the original position
+			  opacity: 1,
+			  rotation: 360,  // Rotate the letters
+			  duration: 1.5,
+			  ease: "back.out(1.7)",
+			  scrollTrigger: {
+				trigger: '.footer',
+				start: 'top bottom',  // Starts when the footer comes into view
+				scrub: 1.9            // Smoothens the animation with scroll
+			  },
+			  stagger: 0.2  // Adds a delay between each letter's animation
+			}
+		  );
+		});
+	  }
+	  
+	  footer();
+	  
+});
